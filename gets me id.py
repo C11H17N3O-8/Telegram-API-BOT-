6927b2f4444aeef21c0dfc1bb987c3054f7b86d8
C11H17N30-8 <76955497+C11H17N3O-8@users.noreply.github.com>
2023-10-03 16:04:38 +0900

    Add files via upload
    "This code is a simple Python script that retrieves a user's chat ID using a Telegram bot. It sets up the Telegram bot token, fetches the latest updates, and prints the user's chat ID. This code is written using the asynchronous asyncio library."
    
    Korean: 먼저 텔레그램의 BOTFATHER의 채널에 들어가서 자신만의 개인 봇을 만듭니다. 그리고 API 토큰을 부여받은 뒤 제공된 코드의 URL란에다 입력하면 개인 봇 채팅방의 ID를 얻을 수 있습니다.
    
    English :"First, go to the BOTFATHER channel on Telegram and create your own personal bot. After obtaining the API token, enter it in the provided code URL to obtain the ID of your personal bot's chat room."

---
+++ gets me id.py
@@ -0,0 +1,28 @@
+import asyncio
+from telegram import Bot
+
+async def get_user_chat_id():
+    # 봇 토큰을 설정합니다.
+    telegram_bot_token = '6626627710:AAGvekwMgs7iIFCiKDegQi_03CWamn8tTy8'
+
+    # 텔레그램 봇 초기화
+    bot = Bot(token=telegram_bot_token)
+
+    # 최신 업데이트 가져오기
+    updates = await bot.get_updates()
+    
+    if updates:
+        # 최신 업데이트 중에서 가장 최근의 업데이트를 선택합니다.
+        latest_update = updates[-1]
+
+        # 사용자 정보 가져오기
+        user_info = latest_update.message.from_user
+
+        # 사용자의 채팅 ID 출력
+        print(f"사용자의 채팅 ID: {user_info.id}")
+    else:
+        print("사용자가 봇에게 아직 메시지를 보내지 않았습니다.")
+
+# 이벤트 루프를 생성하고 비동기 함수 호출
+loop = asyncio.get_event_loop()
+loop.run_until_complete(get_user_chat_id())
